27a5f51002914b25b3458fc2f681cb06773217f1
Terry DuBois <duboisterrence@gmail.com>
2026-01-04 15:13:34 -0800

    Turning branches

--- potted-plants/sketch.js
+++ potted-plants/sketch.js
@@ -3,20 +3,27 @@ let branches;
 
 function setup() {
   createCanvas(1080, 1080);
+  reset();
+}
 
+function reset() {
+
+  // create graphics
   pg = createGraphics(width, height);
   pg.background(200);
 
+  // create branches
   branches = [];
-  for (let i = 0; i < 5; i++) {
-    new Branch();
+  for (let i = 0; i < 1; i++) {
+    new Branch(width / 2, height / 2);
   }
+
+  // reset after 5 seconds
+  setTimeout(reset, 5000);
 }
 
 function randomRange(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.random() * (max - min + 1) + min;
 }
 
 class Branch {
@@ -25,37 +32,59 @@ class Branch {
     this.x = _x;
     this.y = _y;
     this.rad = 10;
-    this.dir = randomRange(0, 360);
-    this.spd = 4;
-    branches.push(this);
+    this.dir = 270;
+    this.dirStart = this.dir;
+    this.dirIncr = 1;
+    this.spd = 1;
+    this.turnSwitches = 0;
 
-    console.log("new branch!");
+    // switch turning direction
+    setInterval(() => {
+      this.turnSwitches++;
+      this.dirIncr = -this.dirIncr;
+    }, 1000);
+
+    branches.push(this);
   }
 
   update() {
 
     // draw self
     pg.fill(0);
-    pg.circle(x, y, rad);
+    pg.circle(this.x, this.y, this.rad);
 
     // move
-    this.x = x + (spd * cos(radians(dir)));
-    this.y = y + (spd * sin(radians(dir)));
+    this.x = this.x + (this.spd * cos(radians(this.dir)));
+    this.y = this.y + (this.spd * sin(radians(this.dir)));
+
+    // turn
+    this.dir += this.dirIncr;
   }
 
 }
 
 function keyPressed() {
 
-  // reset background
+  // reset when pressing R
   if (key === 'r') {
-    pg.reset();
-    pg.background(200);
+    reset();
   }
 }
 
 function draw() {
-  
-  pg.background(200);
+
+  // draw branches
+  for (let i = 0; i < branches.length; i++) {
+    let _branch = branches[i];
+    _branch.update();
+
+    pg.fill('black');
+    pg.rect(10, 10, 100, 60);
+    pg.fill('white');
+    pg.textSize(20);
+    pg.text(round(_branch.x) + ", " + round(_branch.y), 20, 30);
+    pg.text(_branch.turnSwitches, 20, 60);
+  }
+
   image(pg, 0, 0);
 }
